d3c2737311cdabbacfc8524df5deef183657b82d
PIXORA24 <pixora24@gmail.com>
2026-01-16 13:25:32 +0530

    Create index.js

---
+++ core/index.js
@@ -0,0 +1,26 @@
+const events = Object.entries(INVITE_CONFIG.events)
+  .filter(([_, e]) => e.enabled);
+
+if (events.length === 1) {
+  window.location.replace(
+    `invite.html?event=${events[0][0]}`
+  );
+}
+
+const grid = document.getElementById("eventGrid");
+
+events.forEach(([key, event]) => {
+  const card = document.createElement("div");
+  card.className = "event-card";
+
+  card.innerHTML = `
+    <img src="${event.path}preview.jpg">
+    <div class="label">${event.label}</div>
+  `;
+
+  card.onclick = () => {
+    window.location.href = `invite.html?event=${key}`;
+  };
+
+  grid.appendChild(card);
+});
